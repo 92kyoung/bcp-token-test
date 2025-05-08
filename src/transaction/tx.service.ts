bec8f6be00c3176a76e3cc8000de4bc776c7d084
92kyoung <ddowei92@naver.com>
2025-05-08 11:08:47 +0900

    feat : 서버 url 환경변수 처리

--- src/transaction/tx.service.ts
+++ src/transaction/tx.service.ts
@@ -5,15 +5,22 @@ import * as config from 'config';
 
 @Injectable()
 export class TxService {
-  private readonly serverConfig = config.get('web3signerUrl');
+  private readonly serverConfig = config.get('url');
 
+  //utils로 분리
+  public getProvider() {
+    return new ethers.JsonRpcProvider(this.serverConfig.mainNet);
+  }
   public async signAndSend(provider: ethers.JsonRpcProvider, tx: any) {
-    const { data: signedResponse } = await axios.post(this.serverConfig, {
-      jsonrpc: '2.0',
-      method: 'eth_signTransaction',
-      id: 1,
-      params: [tx],
-    });
+    const { data: signedResponse } = await axios.post(
+      this.serverConfig.web3Signer,
+      {
+        jsonrpc: '2.0',
+        method: 'eth_signTransaction',
+        id: 1,
+        params: [tx],
+      },
+    );
 
     if (signedResponse.error) {
       console.error('Web3Signer error:', signedResponse.error);
@@ -24,5 +31,4 @@ export class TxService {
     const txHash = await provider.send('eth_sendRawTransaction', [signedTx]);
     return await provider.waitForTransaction(txHash, 1, 15000);
   }
-
 }
