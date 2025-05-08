274294ec59f1763a649216fd27a9e51a2ebb3415
92kyoung <ddowei92@naver.com>
2025-05-08 11:08:05 +0900

    feat: erc20모듈 프로바이더 추가

--- src/erc20/erc20.module.ts
+++ src/erc20/erc20.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { Erc20Controller } from './erc20.controller';
+import { ERC20Controller } from './erc20.controller';
 import { ERC20Service } from './erc20.service';
+import { TxService } from 'src/transaction/tx.service';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
-  controllers: [Erc20Controller],
-  providers: [ERC20Service]
+  controllers: [ERC20Controller],
+  providers: [ERC20Service, TxService, PrismaService]
 })
-export class Erc20Module {}
+export class ERC20Module {}
