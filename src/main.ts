b19012abf03f74cb4e5ca70ed7a42555c3a829c5
92kyoung <ddowei92@naver.com>
2025-05-08 11:06:31 +0900

    feat : 서버 실행 에러처리 로직 추가

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,23 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
-  const PORT = serverConfig.port || 3000;
-  await app.listen(PORT);
-  Logger.log(`Application is running on Port: ${PORT}`);
+  let PORT = serverConfig.port || 3000;
+
+  const startServer = (port: number) => {
+    return app.listen(port).then(() => {
+      Logger.log(`Application is running on Port: ${port}`);
+    }).catch((err) => {
+      if (err.code === 'EADDRINUSE') {
+        Logger.warn(`Port ${port} is in use. Trying another port...`);
+        startServer(port + 1); // 포트가 사용 중이면 +1 증가시켜서 다시 시도
+      } else {
+        Logger.error(`Error starting the server: ${err.message}`);
+      }
+    });
+  };
+
+  // 서버 실행
+  await startServer(PORT);
 }
+
 bootstrap();
