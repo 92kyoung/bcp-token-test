475ebd0fe43b9a82eda4cc5eae56302bba641744
92kyoung <ddowei92@naver.com>
2025-05-12 16:29:17 +0900

    Refactor : 프리즈마 설정 삭제

--- prisma/migrations/20250430075858_dev/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "ERC20Token" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "symbol" TEXT NOT NULL,
-    "contractAddress" TEXT NOT NULL,
-
-    CONSTRAINT "ERC20Token_pkey" PRIMARY KEY ("id")
-);
