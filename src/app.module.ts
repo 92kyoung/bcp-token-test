735807dc3f3fac98bc64b23ebc9b13f1b887b2bc
92kyoung <ddowei92@naver.com>
2025-05-12 16:29:37 +0900

    feat : 에러 핸들링 영역 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ERC20Module } from './erc20/erc20.module';
+import { Erc721Module } from './erc721/erc721.module';
+import { MetadataModule } from './metadata/metadata.module';
+import { GlobalExceptionsFilter } from './common/exceptions/globlal-exception.filter';
 
 @Module({
-  imports: [ERC20Module],
+  imports: [ERC20Module, Erc721Module, MetadataModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide : 'APP_FILTER',
+      useClass : GlobalExceptionsFilter,
+    }
+  ],
 })
 export class AppModule {}
