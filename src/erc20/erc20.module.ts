d8fbff6959b75b4921e260c18f68c60297b88019
92kyoung <ddowei92@naver.com>
2025-05-12 16:31:55 +0900

    Refactor : 프리즈마 설정 삭제

--- src/erc20/erc20.module.ts
+++ src/erc20/erc20.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ERC20Controller } from './erc20.controller';
 import { ERC20Service } from './erc20.service';
 import { TxService } from 'src/transaction/tx.service';
-import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [ERC20Controller],
-  providers: [ERC20Service, TxService, PrismaService]
+  providers: [ERC20Service, TxService]
 })
 export class ERC20Module {}
